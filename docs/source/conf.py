9d911c7a2847ad097deb1e1b9a34bfee223d6896
koopa31 <clement.cazorla31@gmail.com>
2022-10-13 17:58:41 +0200

    changement du copyright

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -3,8 +3,8 @@
 # -- Project information
 
 project = 'Svetlana'
-copyright = '2022, Cazorla'
-author = 'Cazorla'
+copyright = '2022, Imactiv-3D, CNRS, Université de Toulouse'
+author = 'Imactiv-3D, CNRS, Université de Toulouse'
 
 release = '0.1'
 version = '0.1.0'
